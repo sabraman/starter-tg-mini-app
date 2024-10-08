f3ed034735888fd0e41db13fce1c4681f505ad44
Peng Deng <peng.deng@metatrust.io>
2024-10-08 17:05:10 +0800

    chore: show user name on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Cherry } from "lucide-react";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
+import { getAuth } from "~/lib/security";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const { userData } = await getAuth();
+  const name = userData?.firstName ?? userData?.username ?? "world";
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 px-8">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold">Hello {name}!</h1>
 
       <p className="text-muted-foreground w-full">
         This is a server-side rendered page. This page does not trigger any
