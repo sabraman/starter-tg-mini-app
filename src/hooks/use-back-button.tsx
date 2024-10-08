fb2a7ef875f3ed982e21b19232accdbc53bdd8c8
Peng Deng <peng.deng@metatrust.io>
2024-10-08 16:55:43 +0800

    docs: add docs to the useBackButton and useMainButton hooks

--- src/hooks/use-back-button.tsx
+++ src/hooks/use-back-button.tsx
@@ -2,6 +2,15 @@ import { backButton } from "@telegram-apps/sdk-react";
 import { useRouter } from "next/navigation";
 import { useCallback, useEffect } from "react";
 
+/**
+ * The back button is a native telegram button that is displayed at the top-left
+ * of the screen/view. It is used to navigate back in the app. This hook
+ * activates the back button and sets up an event listener for the click event.
+ * The button will be deactivated during clean-up.
+ *
+ * @param url - The URL to navigate to when the back button is clicked. If not
+ * provided, the browser's default back button behavior will be used.
+ */
 export function useBackButton(url?: string) {
   const router = useRouter();
 
--- src/hooks/use-main-button.tsx
+++ src/hooks/use-main-button.tsx
@@ -1,6 +1,16 @@
 import { mainButton } from "@telegram-apps/sdk-react";
 import { useEffect } from "react";
 
+/**
+ * The main button is a native telegram button that is displayed at the bottom
+ * of the screen. It is used to perform actions in the app. This hook activates
+ * the main button and sets up an event listener for the click event. The button
+ * will be deactivated during clean-up.
+ *
+ * @param onClick - The function to be called when the main button is clicked.
+ * Make sure the function is either defined outside of the react rendering
+ * life-cycle or memoized (using useCallback) to avoid unnecessary re-renders.
+ */
 export function useMainButton(onClick: () => void) {
   useEffect(() => {
     mainButton.setParams({
